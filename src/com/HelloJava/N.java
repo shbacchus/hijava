f8d3a50823bf4f3eb2f658a88ec65b977bd63c40
SHYO <SHYO@DESKTOP-3NO2OA7.mshome.net>
2021-05-15 09:46:14 +0900

    swap 수정

--- src/com/HelloJava/N.java
+++ src/com/HelloJava/N.java
@@ -1,11 +1,28 @@
 package com.HelloJava;
 
 public class N {
+	int x = 10;
+	int y = 20;
 
-	
+	public void swap() {
+		int temp = 0;
+		temp = x;
+		x = y;
+		y = temp;
+	}
 
 	public static void main(String[] args) {
-		int m = 1;
+
+		N a = new N();
+
+		System.out.println("x = " + a.x);
+		System.out.println("y = " + a.y);
+
+		//swap ¼öÁ¤
+		a.swap();
+
+		System.out.println("swap x = " + a.x);
+		System.out.println("swap y = " + a.y);
 
 	}
 }
