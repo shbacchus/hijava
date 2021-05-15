19cb9bff12af7a022ff16fd2fe839ffe31128b6c
SHYO <SHYO@DESKTOP-3NO2OA7.mshome.net>
2021-05-15 09:43:17 +0900

    첫 커밋; first commit;

---
+++ src/com/HelloJava/HelloJava.java
@@ -0,0 +1,14 @@
+package com.HelloJava;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String msg = "Hello Java";
+		say(msg);
+	}
+	public static void say(String msg) {
+		System.out.println(msg);
+	}
+
+}
---
+++ src/com/HelloJava/N.java
@@ -0,0 +1,11 @@
+package com.HelloJava;
+
+public class N {
+
+	
+
+	public static void main(String[] args) {
+		int m = 1;
+
+	}
+}
